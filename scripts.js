7317a0f1c33225a845a9b751a1ec4e5122f7b4d3
wilamc <wilamchen99@gmail.com>
2022-11-29 17:11:44 -0800

    Set up function to create 16x16 grid of squares

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-const container = document.querySelector('#container');
+const defaultSize = 16
+const board = document.querySelector('.board')
+const resetBtn = document.querySelector('.reset');
 
-for (i = 0; i < 17; i++) { // building out several divs using loop
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+createBoard(defaultSize);
+
+function createBoard(size) {
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  for (let i = 0; i < 256; i++) {
+    let squares = document.createElement('div');
+    squares.classList.add('square')
+    squares.style.backgroundColor = 'powderblue';
+    board.insertAdjacentElement("beforeend", squares);
+  }
 }
