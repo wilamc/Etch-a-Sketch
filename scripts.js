219ea921c9f1fd5957c3b2726c47fca825561735
wilamc <wilamchen99@gmail.com>
2022-12-10 10:19:22 -0800

    Began styling page

--- scripts.js
+++ scripts.js
@@ -2,11 +2,12 @@ const defaultSize = 16;
 const board = document.querySelector('.board');
 const resetBtn = document.querySelector('.reset');
 const eraserBtn = document.querySelector('.eraser');
-const penBtn = document.querySelector('.pen')
+const penBtn = document.querySelector('.pen');
+const currentSize = document.querySelector('#size');
 let currentColor = 'black';
 
 resetBtn.onclick = () => resetBoard()
-eraserBtn.onclick = () => changeColor('powderblue')
+eraserBtn.onclick = () => changeColor('white')
 penBtn.onclick = () => changeColor('black')
 
 createBoard(defaultSize);
@@ -14,28 +15,29 @@ createBoard(defaultSize);
 function createBoard(size) {
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-  reloadBoard();
+  removeBoard();
 
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement('div');
     square.addEventListener('mouseover', drawPen)
     square.classList.add('square')
-    square.style.backgroundColor = 'powderblue';
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement("beforeend", square);
+    currentSize.innerText = 'Current Size: ' + size + 'x' + size
   }
 }
 
-function reloadBoard() {
+function removeBoard() {
   let squares = board.querySelectorAll('div');
   squares.forEach((div) => div.remove());
 }
 
 function changeSize(size) {
-  if (size >= 2 && size <= 50) {
+  if (size >= 2 && size <= 64) {
     createBoard(size);
   } else {
-    alert("Please enter a number from 2-50");
+    alert("Please enter a number from 2-64");
   }
 }
 
@@ -52,3 +54,5 @@ function changeColor(color) {
 function resetBoard() {
   location.reload();
 }
+
+currentSize.innerText = 'Current Size: ' + (defaultSize) + 'x' + (defaultSize)
